11c1c56b59feb6c63dceafb1d631ba7efd3caca8
321272 <321272@student.mboutrecht.nl>
2019-07-03 09:36:21 +0200

    initial homepage commit

---
+++ homepages/321272.php
@@ -0,0 +1,44 @@
+<?php
+  echo "Mijn homepage";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sad dog</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+    crossorigin="anonymous">
+</head>
+<body>
+    <html>
+<head>
+
+</head>
+<body>
+<div class="container-fluid">
+  <table>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td><img src="https://scontent-frx5-1.cdninstagram.com/vp/a0f2c0ef86d81d7e66b53396da2fb103/5D84B883/t51.2885-15/e35/60160786_138135207355618_954424021608802057_n.jpg?_nc_ht=scontent-frx5-1.cdninstagram.com&se=7&ig_cache_key=MjA0NDQxNDUyMjgzMTIwMTIxMA%3D%3D.2" alt="Smiley face" height="250" width="250"></td>
+    </tr>
+  </table>
+</div>
+<br>
+<a href="../index.php">terug naar index</a>
+</body>
+</html>
